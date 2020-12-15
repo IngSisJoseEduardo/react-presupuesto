51ef2c46e7d069d23727a9da34ab5917b824e4a7
Jose Eduardo Martinez Perez <jmartinezp@spf.tabasco.local>
2020-12-15 16:27:05 -0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Formulario from './components/Formulario';
+import Pregunta from './components/Pregunta';
+
 
 function App() {
+
+  const [prespuesto, guardarPresupuesto] = useState(0)
+  const [restate, guardarRestante] = useState(0)
+  const [mostrarpregunta, actualizarPregunta] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Gasto semanal</h1>
+      <div className="contenido-principal contenido">
+        <Pregunta 
+          guardarPresupuesto={guardarPresupuesto}
+          guardarRestante={guardarRestante}
+          actualizarPregunta={actualizarPregunta}        />
+
+        <div className="row">
+          <div className="one-half column">
+            <Formulario />
+          </div>
+          <div className="one-half column">
+            2
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Pregunta.js
@@ -0,0 +1,55 @@
+import React,  {Fragment, useState} from 'react';
+import Error from './Error';
+
+const Pregunta = ({guardarPresupuesto, guardarRestante, actualizarPregunta}) => {
+
+    const [cantidad, guardarCantidad] = useState(0);
+    const [error, guardarError] = useState(false);
+
+    const definirPrespuesto = e => {
+        guardarCantidad(Number(e.target.value));
+    };
+
+    // submit para definir el presupuesto
+    const agregarPrespuesto = e => {
+        e.preventDefault();
+
+        // validar
+        if(cantidad < 1 || isNaN( cantidad )) {
+            guardarError(true);
+            return;
+        }
+
+        // si pasa la validacion
+        guardarError(false);
+
+        guardarPresupuesto(cantidad);
+        guardarRestante(cantidad);
+        actualizarPregunta(true);
+    };
+
+    return (
+        <Fragment>
+            <h2>Coloca tu presupuesto</h2>
+            {error? <Error mensaje="El presupuesto es incorrecto"/> :null}
+            <form
+                onSubmit={agregarPrespuesto}
+            >
+                <input
+                    type="number"
+                    className="u-full-width"
+                    placeholder="Coloca tu presupuesto"
+                    onChange={definirPrespuesto}
+                />
+
+                <input
+                    type="submit"
+                    className="button-primary u-full-width"
+                    value="Definir Presupuesto"
+                />
+            </form>
+        </Fragment>
+    );
+}
+ 
+export default Pregunta;
